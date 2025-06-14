77661b96e034a20260d7fdd7426051c8f41fe5c9
william dew <williamrdew@gmail.com>
2025-06-14 19:09:56 +0100

    some minor design edit to mange runs form

--- ProRunnerApp/ManageRunsFrm.Designer.cs
+++ ProRunnerApp/ManageRunsFrm.Designer.cs
@@ -40,21 +40,23 @@
             // panel3
             // 
             panel3.BackColor = Color.Red;
+            panel3.Controls.Add(pictureBox1);
             panel3.Controls.Add(pictureBox2);
             panel3.Controls.Add(label1);
-            panel3.Controls.Add(pictureBox1);
-            panel3.Location = new Point(-26, -1);
+            panel3.Location = new Point(-7, -1);
+            panel3.Margin = new Padding(4, 5, 4, 5);
             panel3.Name = "panel3";
-            panel3.Size = new Size(853, 62);
+            panel3.Size = new Size(941, 103);
             panel3.TabIndex = 3;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.Screenshot_2025_06_11_111743;
             pictureBox2.ImeMode = ImeMode.NoControl;
-            pictureBox2.Location = new Point(724, 0);
+            pictureBox2.Location = new Point(761, 0);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(126, 62);
+            pictureBox2.Size = new Size(180, 103);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
@@ -62,11 +64,12 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Xolonium", 21.75F);
+            label1.Font = new Font("Microsoft Sans Serif", 21.75F);
             label1.ImeMode = ImeMode.NoControl;
-            label1.Location = new Point(343, 12);
+            label1.Location = new Point(360, 21);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(187, 32);
+            label1.Size = new Size(233, 52);
             label1.TabIndex = 1;
             label1.Text = "ProRunner";
             // 
@@ -74,23 +77,26 @@
             // 
             pictureBox1.Image = Properties.Resources.Screenshot_2025_06_11_111743;
             pictureBox1.ImeMode = ImeMode.NoControl;
-            pictureBox1.Location = new Point(3, 0);
+            pictureBox1.Location = new Point(4, 0);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(126, 62);
+            pictureBox1.Size = new Size(180, 103);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
+            pictureBox1.TabIndex = 3;
             pictureBox1.TabStop = false;
             // 
             // ManageRunsFrm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Yellow;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(934, 522);
             Controls.Add(panel3);
-            MaximumSize = new Size(816, 489);
+            Margin = new Padding(4, 5, 4, 5);
+            MaximumSize = new Size(1156, 778);
             Name = "ManageRunsFrm";
             ShowIcon = false;
+            ShowInTaskbar = false;
             SizeGripStyle = SizeGripStyle.Hide;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Manage Runs";
